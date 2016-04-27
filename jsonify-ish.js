5ab4d87386a3b039890e72cbc24fe0307ab5c160
Max Glenister <omgmog@users.noreply.github.com>
2016-04-27 15:16:29 +0100

    Create jsonify-ish.js

---
+++ jsonify-ish.js
@@ -0,0 +1,21 @@
+/*
+
+Crappy devtools method for getting all of the custom emoji out of slack...
+
+Will give you the following for each:
+
+  { "url": "url to image", "filename": "filename.ext" },
+
+*/
+
+
+var emoji = document.querySelectorAll('.emoji_row .emoji-wrapper:first-child'); 
+
+for (var i=0;i<emoji.length;i++) {
+  var url = emoji[i].dataset.original;
+  var ext = url.split('.').pop();
+  var text = emoji[i].parentElement.nextSibling.nextSibling.innerText.replace(':','').replace(':','');
+
+  console.log('{ "url": "'+url+'", "filename":"'+text+'.'+ext+'"},');
+
+};
