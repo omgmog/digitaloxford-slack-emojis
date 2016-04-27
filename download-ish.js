6e5faf2c80da2ab2f9857a953590da1cc6ce14ea
Max Glenister <omgmog@users.noreply.github.com>
2016-04-27 15:17:51 +0100

    Create download-ish.js

---
+++ download-ish.js
@@ -0,0 +1,26 @@
+/*
+
+Crappy node.js way of downloading all of the emoji
+
+This assumes you've got a json file called emoji.json
+
+*/
+
+var fs = require('fs');
+var request = require('request');
+
+var download = function (url, filename, callback) {
+  request.head(url, function (error, response, body) {
+    request(url).pipe(fs.createWriteStream(filename)).on('close', callback);
+  });
+};
+
+var files = require('./emoji.json');
+
+for (var i=0;i<files.length;i++) {
+  var url = files[i].url;
+  var file = files[i].filename;
+  download(url, file, function () {
+    console.log('Downloaded', file);
+  });
+};
