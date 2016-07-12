ec9fb298ecd6a008734c8d4a5c21acb7303f13b6
Max Glenister <omgmog@users.noreply.github.com>
2016-07-12 10:55:08 +0100

    Update jsonify-ish.js

--- jsonify-ish.js
+++ jsonify-ish.js
@@ -10,12 +10,15 @@ Will give you the following for each:
 
 
 var emoji = document.querySelectorAll('.emoji_row .emoji-wrapper:first-child'); 
+var out = [];
 
 for (var i=0;i<emoji.length;i++) {
   var url = emoji[i].dataset.original;
   var ext = url.split('.').pop();
   var text = emoji[i].parentElement.nextSibling.nextSibling.innerText.replace(':','').replace(':','');
 
-  console.log('{ "url": "'+url+'", "filename":"'+text+'.'+ext+'"},');
+  out.push({ "url": "'+url+'", "filename":"'+text+'.'+ext+'"});
 
 };
+// Gives us one big JSON object, rather than a load of pseudo-JSON objects
+console.log(JSON.stringify(out));
